168c000a62586ea3197ed997c4f0951a0bad412a
sht <devshtg@gmail.com>
2023-06-29 21:27:38 -0300

    Sht

---
+++ src/pages/Welcome/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { button } from "react-native";
+import { 
+    View, 
+    Text,
+    StyleSheet
+} from "react-native";
+
+export default function Welcome(){
+    return(
+        <View style={style.options}>
+            <View>
+                
+            </View>
+        </View>
+    );
+}
+
+const style = StyleSheet.create({
+    options:{
+        backgroundColor:"#219EBC",
+    }
+})
---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import Welcome from '../pages/Welcome'
+import Pesquisa from '../pages/Pesquisa';
+
+
+const Stack = createNativeStackNavigator()
+
+export default function Routes(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen 
+                name="Welcome"
+                component={Welcome}
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen 
+                name="Pesquisa"
+                component={Pesquisa}
+            />
+        </Stack.Navigator>
+    );
+}
