d05e255ffd6e44ecd850c6f39f9e8766d7151e18
sht <devshtg@gmail.com>
2023-06-29 22:33:49 -0300

    att

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,11 +1,69 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Image,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
+
 
 export default function Welcome(){
     return(
-        <View>
-            <Text>Musicas</Text>
+        <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Text style={styles.titleStyle}>
+          Botão Flutuante
+        </Text>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={styles.touchableOpacityStyle}>
+          <Image
+            source={{
+              uri:
+                'https://developerplus.com.br/wp-content/uploads/2021/12/plus_icon.png',
+            }}
+            style={styles.floatingButtonStyle}
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-        </View>
-    );
-}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'white',
+      padding: 10,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    titleStyle: {
+      fontSize: 28,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      padding: 10,
+    },
+    textStyle: {
+      fontSize: 16,
+      textAlign: 'center',
+      padding: 10,
+    },
+    touchableOpacityStyle: {
+      position: 'absolute',
+      width: 50,
+      height: 50,
+      alignItems: 'center',
+      justifyContent: 'center',
+      right: 30,
+      bottom: 30,
+    },
+    floatingButtonStyle: {
+      resizeMode: 'contain',
+      width: 70,
+      height: 70,
+    },
+  });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,8 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-
 import Welcome from '../pages/Welcome'
-import Pesquisa from '../pages/Pesquisa'
-
 
 const Stack = createNativeStackNavigator()
+
 
 export default function Routes(){
     return(
@@ -12,12 +10,7 @@ export default function Routes(){
             <Stack.Screen 
                 name="Welcome"
                 component={Welcome}
-                options={{ headerShown: false }}
-            />
 
-            <Stack.Screen 
-                name="Pesquisa"
-                component={Pesquisa}
             />
         </Stack.Navigator>
     );
